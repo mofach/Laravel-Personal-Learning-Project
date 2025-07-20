402a92588edc66ed3af9fbdd4ca091861c6e25f5
mofach <aqilmf379@gmail.com>
2025-07-20 23:30:07 +0700

    Add Relation Table Post-Category

--- dasar-filament/app/Models/Post.php
+++ dasar-filament/app/Models/Post.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Post extends Model
 {
     use HasFactory;
 
@@ -15,17 +15,19 @@ class Category extends Model
      * @var array
      */
     protected $fillable = [
-        'name',
-        'description',
+        'category_id',
+        'image',
+        'title',
+        'content'
     ];
 
     /**
-     * posts
+     * category
      *
      * @return void
      */
-    public function posts()
+    public function category()
     {
-        return $this->hasMany(Post::class);
+        return $this->belongsTo(Category::class);
     }
 }
